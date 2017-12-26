09acad0d79344c1a7b10ceb724d91312a63ce062
kishsr <kishsr@DIN76000835>
2017-12-26 12:35:47 +0530

    Add project files.

---
+++ MvcTutorial/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult Laptop()
+        {
+            return View();
+        }
+
+        public ActionResult TV()
+        {
+            return View();
+        }
+
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+
+        public ActionResult Microwave()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcTutorial/Controllers/ServicesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+
+        public ActionResult Servicing()
+        {
+            return View();
+        }
+        public ActionResult LaptopServicing()
+        {
+            return View();
+        }
+
+        public ActionResult TVServicing()
+        {
+            return View();
+        }
+
+        public ActionResult MobileServicing()
+        {
+            return View();
+        }
+
+        public ActionResult MicrowaveServicing()
+        {
+            return View();
+        }
+    }
+}
